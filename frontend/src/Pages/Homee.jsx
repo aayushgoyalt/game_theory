f50e73456dab59460185ce07306a3268792258df
Aayush <agoyal1_be23@thapar.edu>
2024-11-12 15:59:35 +0530

    first commit

---
+++ frontend/src/Pages/Homee.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Homee = () => {
+  return (
+    <div className="flex bg-black w-full justify-center items-center">
+      <img
+        src="../../Public/home.png"
+        className=" border border-white h-[100vh]"
+        alt=""
+      />
+      <button className="z-50  absolute text-4xl mt-[270px]  xl:mt-[300px] px-[170px] py-5 ">
+        LOG IN
+      </button>
+    </div>
+  );
+};
+
+export default Homee;
